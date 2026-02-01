a54209975eff1b066f2693d0160f38b2c8c98ee2
FelipeMovio <felipelsmovio@gmail.com>
2026-01-31 22:18:19 -0300

    inicial class Url

--- src/main/java/com/URL/demo/entities/Url.java
+++ src/main/java/com/URL/demo/entities/Url.java
@@ -1,6 +1,12 @@
 package com.URL.demo.entities;
 
+import jakarta.persistence.*;
 
-
+@Entity
+@Table(name = "urls")
 public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
