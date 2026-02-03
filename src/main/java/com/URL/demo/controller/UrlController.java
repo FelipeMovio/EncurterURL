66237fcc79b3d16a3f523bc6c5029bba4c492799
FelipeMovio <felipelsmovio@gmail.com>
2026-02-03 20:17:57 -0300

    update

--- src/main/java/com/URL/demo/controller/UrlController.java
+++ src/main/java/com/URL/demo/controller/UrlController.java
@@ -1,12 +1,15 @@
 package com.URL.demo.controller;
 
 import com.URL.demo.DTOs.shortUrlDTORequet;
+import com.URL.demo.entities.Url;
 import com.URL.demo.repositoy.UrlRepository;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class UrlController {
@@ -21,6 +24,8 @@ public class UrlController {
             id = RandomStringUtils.randomAlphanumeric(5, 10);
 
         }while (repository.existsById(id));
+
+        repository.save(new Url(id,request.url()));
 
         return ResponseEntity.ok().build();
     }
