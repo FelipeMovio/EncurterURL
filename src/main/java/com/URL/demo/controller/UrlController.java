9dc30be2d4f17dd45e2c433b7f9ba3d5160b3e72
FelipeMovio <felipelsmovio@gmail.com>
2026-02-03 20:10:45 -0300

    update

--- src/main/java/com/URL/demo/controller/UrlController.java
+++ src/main/java/com/URL/demo/controller/UrlController.java
@@ -1,6 +1,7 @@
 package com.URL.demo.controller;
 
 import com.URL.demo.DTOs.shortUrlDTORequet;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,8 @@ public class UrlController {
 
     @PostMapping("/shortURL")
     public ResponseEntity<Void> shortURL(@RequestBody shortUrlDTORequet request){
+
+        var id = RandomStringUtils.randomAlphanumeric(5, 10);
 
         return ResponseEntity.ok().build();
     }
