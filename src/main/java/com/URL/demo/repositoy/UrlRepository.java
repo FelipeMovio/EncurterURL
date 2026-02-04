a0c2efa7ea3b473cbd49abc7c5015fdcbd6e5997
FelipeMovio <felipelsmovio@gmail.com>
2026-02-04 19:10:36 -0300

    update

--- src/main/java/com/URL/demo/repositoy/UrlRepository.java
+++ src/main/java/com/URL/demo/repositoy/UrlRepository.java
@@ -2,6 +2,8 @@ package com.URL.demo.repositoy;
 
 import com.URL.demo.entities.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UrlRepository extends JpaRepository<Url,String> {
 }
