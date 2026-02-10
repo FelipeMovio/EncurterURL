779940ee148ac34f3c156be54790013cad7dea12
FelipeMovio <felipelsmovio@gmail.com>
2026-02-10 19:23:25 -0300

    ...

--- src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,14 @@
 package com.URL.demo.exceptions;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler  {
+
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    public ResponseEntity<?> HandleRecursoNaoEncontrado(RecursoNaoEncontradoException ex){
+
+    }
 }
