f7de19789b7ccebf22adbadbf6a50be2f1b4c0d4
FelipeMovio <felipelsmovio@gmail.com>
2026-02-11 18:33:20 -0300

    ... update

--- src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.URL.demo.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -13,6 +15,10 @@ public class GlobalExceptionHandler  {
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<?> HandleRecursoNaoEncontrado(RecursoNaoEncontradoException ex){
         Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("Error","Recurso nao encontrado ");
+        body.put("message", ex.getMessage());
 
     }
 }
