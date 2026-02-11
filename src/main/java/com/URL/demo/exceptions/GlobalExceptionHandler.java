c43e6e5574cb38448440f1e963388c0d22257a96
FelipeMovio <felipelsmovio@gmail.com>
2026-02-11 18:29:27 -0300

    Map<String, Object> body = new LinkedHashMap<>();

--- src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/URL/demo/exceptions/GlobalExceptionHandler.java
@@ -4,11 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler  {
 
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<?> HandleRecursoNaoEncontrado(RecursoNaoEncontradoException ex){
+        Map<String, Object> body = new LinkedHashMap<>();
 
     }
 }
